d5ca7b23c79cfac9bcaceff428fd3d5212be8648
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-07 01:46:53 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,6 +9,7 @@ variable "subnet_ocid" {}
 variable "network_security_group_id" {}
 variable "ssh_private_key" {}
 variable "ssh_public_key" {}
+variable "instance_shape" {}
 variable "is_testing_required" {}
 variable "is_website_installed" {}
 variable "is_loadbalancer_installed" {}
