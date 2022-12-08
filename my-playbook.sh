15ead0153a0711be05f3f2ccd24767115fd06f16
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-07 17:19:21 -0800

    Update my-playbook.sh

--- my-playbook.sh
+++ my-playbook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-ansible-playbook playbook.yaml  -i tf_inventory.yaml -u opc --key-file /Users/mguiagou/.jenkins/cloudshellkey > log.out
+/usr/local/bin/ansible-playbook /Users/mguiagou/terraform-ws/demo/tensorflow-demo-sanjose/playbook.yaml  -i /Users/mguiagou/terraform-ws/demo/tensorflow-demo-sanjose/tf_inventory.yaml -u opc --key-file /Users/mguiagou/.jenkins/cloudshellkey > log.out
