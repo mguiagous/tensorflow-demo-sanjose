9429d6c8cc38e66f0e01e6eba14b2ee601ed5b08
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-07 16:47:42 -0800

    Update my-playbook.sh

--- my-playbook.sh
+++ my-playbook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-ansible-playbook playbook.yaml  -i tf_inventory.yaml -u opc --key-file ~/.ssh/cloudshellkey  > output.txt
+ansible-playbook playbook.yaml  -i tf_inventory.yaml -u opc --key-file ~/.jenkins/cloudshellkey > log.out
