673b0618846c12f79fbe99a5d6a2f37e48ec9e7c
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-07 16:48:52 -0800

    Update my-playbook.sh
    added jenkens path for key

--- my-playbook.sh
+++ my-playbook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-ansible-playbook playbook.yaml  -i tf_inventory.yaml -u opc --key-file ~/.jenkins/cloudshellkey > log.out
+ansible-playbook playbook.yaml  -i tf_inventory.yaml -u opc --key-file /Users/mguiagou/.jenkins/cloudshellkey > log.out
