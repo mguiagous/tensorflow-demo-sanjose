6c8d5888c492ce7ebff3af793007547c1eab3b79
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-07 17:28:04 -0800

    Delete my-playbook.sh

--- my-playbook.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-/usr/local/bin/ansible-playbook /Users/mguiagou/terraform-ws/demo/tensorflow-demo-sanjose/playbook.yaml  -i /Users/mguiagou/terraform-ws/demo/tensorflow-demo-sanjose/tf_inventory.yaml -u opc --key-file /Users/mguiagou/.jenkins/cloudshellkey > log.out
