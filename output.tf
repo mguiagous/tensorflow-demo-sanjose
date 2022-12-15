e014b0e2c41f67020ce4ca7d0f55b26ed4de8f07
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-14 19:35:38 -0800

    Delete output.tf

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "display_name" { value = oci_core_instance.this.*.display_name }
-output "public_ip_address" { value = oci_core_instance.this.*.public_ip }
-output "private_ip_address" { value = oci_core_instance.this.*.private_ip }
-output "testing_required" { value = var.is_testing_required }
-output "weapp_installed" { value = var.is_website_installed }
-output "Load-Balancer-IPs" { value = oci_load_balancer.this.*.ip_addresses }
-output "LB-Max_Bandwith_in_mgps" { value = var.lb_max_bandwith_in_mgps }
-output "LB-Min_Bandwith_in_mgps" { value = var.lb_min_bandwith_in_mgps }
