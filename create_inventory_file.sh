8f8ae6bd8f4cad73da87cfdf835814eef300c12e
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2022-12-14 23:00:35 -0800

    Update create_inventory_file.sh

--- create_inventory_file.sh
+++ create_inventory_file.sh
@@ -3,18 +3,18 @@
 
 echo -e "\n Checking to see if the inventory file /home/mail2maham/demo1/ansible/tf_inventory.yaml exisits"
 
-if [ -f "/home/mail2maham/demo1/ansible/tf_inventory.yaml" ]; 
+if [ -f "~/tensorflow-demo-sanjose/tf_inventory.yaml" ]; 
 then
-    echo -e "\n Found /home/mail2maham/demo1/ansible/tf_inventory.yaml, it will be deleted"
-    rm /home/mail2maham/demo1/ansible/tf_inventory.yaml
+    echo -e "\n Found ~/tensorflow-demo-sanjose/tf_inventory.yaml, it will be deleted"
+    rm ~/tensorflow-demo-sanjose/tf_inventory.yaml
     echo -e "\n Checking to verify that it is deleted ..... "
     pwd
     ls -ltr
 fi
-echo -e "\n Creating the /home/mail2maham/demo1/ansible/tf_inventory.yaml now ...." 
-touch /home/mail2maham/demo1/ansible/tf_inventory.yaml
-echo -e "[servers]" > /home/mail2maham/demo1/ansible/tf_inventory.yaml
+echo -e "\n Creating the ~/tensorflow-demo-sanjose/tf_inventory.yaml now ...." 
+touch ~/tensorflow-demo-sanjose/tf_inventory.yaml
+echo -e "[servers]" > ~/tensorflow-demo-sanjose/tf_inventory.yaml
 pwd
 ls -ltr
 
-cat /home/mail2maham/demo1/ansible/tf_inventory.yaml
+cat ~/tensorflow-demo-sanjose/tf_inventory.yaml
