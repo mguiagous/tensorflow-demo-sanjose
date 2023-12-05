c51611ccdb3dcb47efb2bbbb3c6b89a446ca3231
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2023-12-04 22:47:21 -0800

    Update test_ssh.tf
    Added SSH failed attempts

--- test_ssh.tf
+++ test_ssh.tf
@@ -41,3 +41,53 @@ resource "null_resource" "ssh-tester" {
   }
 }
 
+
+
+
+resource "null_resource" "olinux-ssh-attempt" {
+  depends_on = [oci_core_instance.this]
+  count      = (var.is_testing_required) ? var.nb_of_ssh_attempts : 0
+
+  connection {
+    agent       = false
+    timeout     = "30m"
+    host        = oci_core_instance.this[count.index].public_ip
+    user        = "opc"
+    private_key = file(var.ssh_private_key)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ",
+      "echo \"#####################################\"",
+      "echo \" SERVER SUCCESSFULLY CONNECTED. .......\"",
+      "echo \"#####################################\"",
+    ]
+  }
+}
+
+
+resource "null_resource" "olinux-ssh-access-failed-attempts" {
+
+  depends_on = [oci_core_instance.this]
+  count      = (var.is_testing_required) ? var.nb_of_failed_ssh_attempts : 0
+
+  connection {
+    timeout     = "1m"
+    host        = oci_core_instance.mongodb-olinux[count.index].public_ip
+    user        = "opc"
+    private_key = file(var.ssh_private_key0)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ",
+      "echo \"############################################################\"",
+      "echo \" UNSUCCESSFUL SSH CONNECTION ATTEMPTED .....\"",
+      "echo \"############################################################\"",
+      "echo ",
+    ]
+  }
+}
+
+
